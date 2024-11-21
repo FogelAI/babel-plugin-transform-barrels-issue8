1127a81e4c14feca29ac327ae555c069ecb04ce6
Ori <fogelori@gmail.com>
2024-11-21 23:50:44 +0200

    update mock and requireActual to next-i18next

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,14 +1,22 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 
-jest.mock('react-i18next', () => ({
-    ...jest.requireActual('react-i18next'),
-    useTranslation: (domain?: string) => {
-      return {
-        t: (str: string) => str,
-        i18n: {
-          changeLanguage: () => new Promise(() => {}),
-        },
-      };
+jest.mock('next-i18next', () => ({
+  ...jest.requireActual('next-i18next'),
+  useTranslation: () => ({
+    t: (key: string, args: unknown) => {
+      if (args != null) {
+        const keys = Object.keys(args);
+
+        if (keys.length >= 1 && keys[0] !== 'ns') {
+          return `${key} - ${JSON.stringify(args)}`;
+        }
+      }
+
+      return key;
     },
+    i18n: {
+      language: 'en-US',
+    },
+  }),
 }));
